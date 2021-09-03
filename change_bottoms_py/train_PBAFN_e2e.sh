c3b358d50ed0f42fa5a4a757269f07a819d2d122
kim-jeonghyun <kimjeonghyun.jkim@gmail.com>
2021-09-03 15:58:14 +0900

    Update train_PBAFN_e2e.sh
    이전 업데이트에서 반영하지 못한 수정 사항을 더했습니다.

--- change_bottoms_py/train_PBAFN_e2e.sh
+++ change_bottoms_py/train_PBAFN_e2e.sh
@@ -1,4 +1,4 @@
-python -m torch.distributed.launch --nproc_per_node=1 --master_port=4736 train_PBAFN_e2e.py --name PBAFN_e2e --PBAFN_warp_checkpoint 'checkpoints/PBAFN_stage1/PBAFN_warp_epoch_101.pth' --resize_or_crop None --verbose --tf_log --batchSize 8 --num_gpus 1 --label_nc 21 --launcher pytorch --save_epoch_freq 2
+python -m torch.distributed.launch --nproc_per_node=1 --master_port=4736 train_PBAFN_e2e.py --name PBAFN_e2e --PBAFN_warp_checkpoint 'checkpoints/PBAFN_stage1/PBAFN_warp_epoch_101.pth' --resize_or_crop None --verbose --tf_log --batchSize 8 --num_gpus 1 --label_nc 21 --launcher pytorch --save_epoch_freq 10 # --continue_train
 
 
 
