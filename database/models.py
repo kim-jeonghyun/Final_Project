09504040618821cff33689811e959f894a8b7e26
Jinyong Lee <65713225+yn-e-si@users.noreply.github.com>
2021-09-05 16:46:24 +0900

    change the database schema

--- database/models.py
+++ database/models.py
@@ -3,16 +3,11 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
-class Top_item(db.Model):
-    __tablename__ = "top_items"
-    top_id = db.Column(db.Integer, primary_key = True)
+class Itesm(db.Model):
+    __tablename__ = "items"
+    item_id = db.Column(db.Integer, primary_key = True)
     item = db.Column(db.String, nullable=False)
-    sex = db.Column(db.String, nullable=False)
-
-class Bottom_item(db.Model):
-    __tablename__ = "bottom_items"
-    bottom_id = db.Column(db.Integer, primary_key = True)
-    item = db.Column(db.String, nullable=False)
+    category = db.Column(db.String, nullable=False)
     sex = db.Column(db.String, nullable=False)
 
 class User(db.Model):
@@ -20,31 +15,12 @@ class User(db.Model):
     user_id = db.Column(db.Integer, primary_key = True)
     user_key = db.Column(db.Integer, nullable=False)
     model_image = db.Column(db.String, default='~~~.jpg')
+    generate = db.Column(db.Boolean, unique=False, default = False)
 
-class Model(db.Model):
-    __tablename__ = 'model'
-    model_id = db.Column(db.Integer, primary_key = True)
-    top_items = db.Column(db.String, db.ForeignKey('top_items.item'), nullable=True)
-    bottom_items = db.Column(db.String, db.ForeignKey('bottom_items.item'), nullable=True)
+class Generate(db.Model):
+    __tablename__ = 'generate'
+    generate_id = db.Column(db.Integer, primary_key = True)
     user_key = db.Column(db.Integer, db.ForeignKey('user.user_key'), nullable=False)
+    item = db.Column(db.String, db.ForeignKey('items.item'), nullable=True)
     model_image = db.Column(db.String, db.ForeignKey('user.model_image'), nullable=False)
-    top_generate_image = db.Column(db.String, db.ForeignKey('top_generate.top_generate_image'), nullable=True)
-    bottom_generate_image = db.Column(db.String, db.ForeignKey('bottom_generate.bottom_generate_image'), nullable=True)
-
-class Top_generate(db.Model):
-    __tablename__ = 'top_generate'
-    top_generate__id = db.Column(db.Integer, primary_key = True)
-    user_key = db.Column(db.Integer, db.ForeignKey('user.user_key'), nullable=False)
-    model_image = db.Column(db.String, db.ForeignKey('user.model_image'), nullable=True)
-    item = db.Column(db.String, db.ForeignKey('top_items.item'), nullable=False)
-    top_generate_image = db.Column(db.String, nullable = False)
-    bottom_generate_image = db.Column(db.String, db.ForeignKey('bottom_generate.bottom_generate_image'), nullable=True)
-
-class Bottom_generate(db.Model):
-    __tablename__ = 'bottom_generate'
-    bottom_generate_id = db.Column(db.Integer, primary_key = True)
-    user_key = db.Column(db.Integer, db.ForeignKey('user.user_key'), nullable=False)
-    model_image = db.Column(db.String, db.ForeignKey('user.model_image'), nullable=True)
-    item = db.Column(db.String, db.ForeignKey('bottom_items.item'), nullable=False)
-    top_generate_image = db.Column(db.String, db.ForeignKey('top_generate.top_generate_image'), nullable=True)
-    bottom_generate_image = db.Column(db.String, nullable=False)
+    generate_image = db.Column(db.String, nullable=False)
