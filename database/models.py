f55744f27353f5a0edb97a2b9a3fe3ba8910068c
Jinyong Lee <65713225+yn-e-si@users.noreply.github.com>
2021-09-05 18:53:22 +0900

    Change Generate table

--- database/models.py
+++ database/models.py
@@ -6,7 +6,7 @@ db = SQLAlchemy()
 class Itesm(db.Model):
     __tablename__ = "items"
     item_id = db.Column(db.Integer, primary_key = True)
-    item = db.Column(db.String, nullable=False)
+    item = db.Column(db.String, unique=True, nullable=False)
     category = db.Column(db.String, nullable=False)
     sex = db.Column(db.String, nullable=False)
 
@@ -14,13 +14,12 @@ class User(db.Model):
     __tablename__ = "user"
     user_id = db.Column(db.Integer, primary_key = True)
     user_key = db.Column(db.Integer, nullable=False)
-    model_image = db.Column(db.String, default='~~~.jpg')
+    model_image = db.Column(db.String, , unique=True, default='~~~.jpg')
     generate = db.Column(db.Boolean, unique=False, default = False)
 
 class Generate(db.Model):
     __tablename__ = 'generate'
     generate_id = db.Column(db.Integer, primary_key = True)
-    user_key = db.Column(db.Integer, db.ForeignKey('user.user_key'), nullable=False)
     item = db.Column(db.String, db.ForeignKey('items.item'), nullable=True)
     model_image = db.Column(db.String, db.ForeignKey('user.model_image'), nullable=False)
     generate_image = db.Column(db.String, nullable=False)
