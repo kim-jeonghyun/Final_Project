92bd4ca903b19b706c33a89a85fae3059736ede2
Jinyong Lee <65713225+yn-e-si@users.noreply.github.com>
2021-09-08 22:31:04 +0900

    edit db schema and api

--- flask_demo/server.py
+++ flask_demo/server.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, request, jsonify
 from sqlalchemy import create_engine, text
 from werkzeug.utils import secure_filename
-from sign_up import insert_user, get_user, insert_user_key
+from sign_up import insert_generate, insert_user, get_user, insert_user_key
 from connection import s3_connection
 from s3_config import BUCKET_NAME
 from make_user_key import get_user_key
@@ -40,36 +40,42 @@ def render_file():
 @app.route('/fileUpload', methods = ['GET', 'POST'])
 def upload_file():
     if request.method == 'POST':
+
+        # 이미지 객체
         f = request.files['file']
+        
 
 
         ###########################
         ##### 이미지 전처리 #######
         ###########################
 
+        # upload 시 user_key 생성 -> 파일 이름 규칙을 위해
         user_key = get_user_key(32)
-        insert_user_key(user_key)
+
+
 
         #저장할 경로 + 파일명
-        top_id, bottom_id = 0, 0
+
+        # 처음 모델 업로드 시, 항상 상의: 0 , 하의: 0
+        top_id, bottom_id = 00, 00
         s3_path = 'model-image/' + user_key + str(top_id) + str(bottom_id)
+
         s3.put_object(
             Bucket = BUCKET_NAME,
             Body = f,
             Key = s3_path,
             ContentType = f.content_type
-            )
-        location = s3.get_bucket_location(Bucket=BUCKET_NAME)['LocationConstraint']
-        image_url = f'https://{BUCKET_NAME}.s3.{location}.amazonaws.com/{s3_path}'
+        )
+    location = s3.get_bucket_location(Bucket=BUCKET_NAME)['LocationConstraint']
+    image_url = f'https://{BUCKET_NAME}.s3.{location}.amazonaws.com/{s3_path}'
 
-        new_user ={ 
-            'user_key': user_key,
-            'model_image': image_url,
-            'generate': False
-            }
+    new_user ={ 
+        'model_url': image_url,
+        'generate': False
+        }
 
-        new_user_id = insert_user(new_user)
-
+    insert_user(new_user)
 
     return render_template('view.html', image_url = image_url)
     #return 'uploads 디렉토리 -> 파일 업로드 성공!' + image_url
@@ -79,20 +85,62 @@ def upload_file():
 
 # generate HTML 렌더링
 # 상의 or 하의 item 선택 시 작동
-#@app.route('/generate')
-#def render_file():
-   # 1. 해당 
-   # 이미지를 클릭할 시 해당 item 의 id 받아오기
+@app.route('/generate')
+def render_file():
+    # 1. 이미지를 클릭할 시 해당 item 의 id 받아오기
+    # image_url 과 item id 가 넘어옴
+    # 1. 해당 url 이 user 테이블에 있는 지 조회
+    # 2. 만약 있다면 바로 s3 버켓에서 해당 url 이미지 불러오기
+    # 3. 없다면 딥러닝 모델을 거쳐 나온 아웃풋을 db에 저장하고 이를 불러오기
+    model_url = model_url
+    item_url = item_url
+    item_num = get_item_num(item_url)
+    if check_category(item_url) == 'top':
+        new_model_url = model_url[:-4] + str(item_num) + model_url[-2:]
+    else:
+        new_model_url = model_url[:-2] + str(item_num)
+    
+    
+    if check_model(new_model_url):
+        return new_model_url
+    else:
+        item_url = item_url
+        mask_url = get_mask_url()
+
+        # model(item_url, mask_url, model_url)
+
+        ####################
+        ######모델추론######
+        ####################
+        # output_image = model.predict()
+
+        s3_path = new_model_url.split('/')[-2] + '/'+ new_model_url.split('/')[-1]
+        s3.put_object(
+            Bucket = BUCKET_NAME,
+            Body = output_image,
+            Key = s3_path,
+            ContentType = output_image.content_type
+        )
+        location = s3.get_bucket_location(Bucket=BUCKET_NAME)['LocationConstraint']
+        image_url = f'https://{BUCKET_NAME}.s3.{location}.amazonaws.com/{s3_path}'
+        gernerate = {
+            'model_url' = model_url,
+            'item_number' = item_num,
+            'generate_url' = new_model_url
+            }
+
+        user = {
+            'model_url' = new_model_url,
+            'generate' = True
+            }
+
+        insert_generate(generate)
+        
+        insert_user(user)
+
+        return generate_url
 
 
-
-
-   ####################
-   ######모델추론######
-   ####################
-
-
-   #return render_template('generate.html')
 
 
 
--- flask_demo/sign_up.py
+++ flask_demo/sign_up.py
@@ -26,74 +26,98 @@ def get_user(user_id):
 def insert_user(user):
     return current_app.database.execute(text("""
         INSERT INTO User (
-            user_key,
-            model_image,
+            model_url,
             generate
         ) VALUES (
-            :user_key,
-            :model_image,
+            :model_url,
             :generate
         )
-    """), user).lastrowid#.inserted_primary_key # 새로 사용자가 생성되면 새로 생성된 사용자의 아이디를 읽어들인다.
-
-def insert_user_key(user_key):
-    current_app.database.execute(text("""
-        INSERT INTO User_key(
-            user_key
-        ) VALUES (
-            :user_key
-        )
-        """), {
-            'user_key': user_key
-            })
+    """), user)#.lastrowid#.inserted_primary_key # 새로 사용자가 생성되면 새로 생성된 사용자의 아이디를 읽어들인다.
 
 
 def insert_item(item):
     current_app.database.execute(text("""
         INSERT INTO Item (
-            item,
+            item_number,
+            item_url,
             category,
             sex
         ) VALUES (
-            :item,
+            :item_number,
+            :item_url,
             :category,
             :sex
         )
         """), item)
 
-def insert_top_item(top_item):
+def insert_mask(mask):
     current_app.database.execute(text("""
         INSERT INTO Item (
-            top_item
+            item_number,
+            mask_url
         ) VALUES (
-            :top_item
+            :item_number,
+            :mask_url,
         )
-        """),   {
-        'top_item' : top_item
-    })
+        """), mask)
 
-def insert_bottom_item(bottom_item):
-    current_app.database.execute(text("""
-        INSERT INTO Bottom (
-            bottom_item
-        ) VALUES (
-            :bottom_item
-        )
-        """),  {
-        'bottom_item' : bottom_item
-    })
 
 def insert_generate(generate):
     return current_app.database.execute(text("""
         INSERT INTO Generate(
-            model_image,
-            top_item,
-            bottom_item,
-            generate_image,
+            model_url,
+            item_number,
+            generate_url,
             ) VALUES (
-                : model_image,
-                : top_item,
-                : bottom_item,
-                : generate_image
+                :model_url,
+                :item_number
+                :generate_url
             )
         """), generate)
+
+
+def check_model(model_url):
+    image = current_app.database.execute(text("""
+        SELECT
+            model_url
+        FROM User
+        WHERE model_url =:model_url
+        """), {
+            'model_url' :model_url
+        }).fetchone()
+
+    return True if image else None
+
+
+def check_category(item_url):
+    item = current_app.database.execute(text("""
+        SELECT
+            category
+        FROM Item
+        WHERE item_url =:item_url
+        """), {
+            'item_url' :item_url
+        }).fetchone()
+    return item['category']
+
+def get_item_num(item_url):
+    item = current_app.database.execute(text("""
+        SELECT
+            item_number
+        FROM Item
+        WHERE item_url =:item_url
+        """), {
+            'item_url' :item_url
+        }).fetchone()
+    return item['item_number']
+
+def get_mask_url(item_num):
+    mask = current_app.database.execute(text("""
+        SELECT
+            mask_url
+        FROM Mask
+        WHERE item_num =:item_num
+        """), {
+            'item_num' :item_num
+        }).fetchone()
+    return mask['mask_url']
