677be1de23bc9e1394f28fa568b939256270dfbb
kim-jeonghyun <kimjeonghyun.jkim@gmail.com>
2021-09-14 23:04:31 +0900

    Add files for web sites
    웹사이트 구축을 위한 css, html, js 파일을 추가하였습니다.

--- JH_web_demo_v2/run_model.py
+++
@@ -1,9 +0,0 @@
-from PFAFN.test import RunModel
-
-def matching_filename(model, clothes):
-    text = open('PFAFN/demo.txt', 'w')
-    text.write(model + ' ' + clothes)
-    return text
-
-def run(baseroot, output_path):
-    RunModel().run_model(baseroot, output_path)
